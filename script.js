f1b7399eccdef1a3011ff13cad88aba215c06cb8
JoeCrocker <joe.crocker@immersivelabs.co.uk>
2020-12-08 18:22:06 +0000

    adding starter files for background generator

---
+++ script.js
@@ -0,0 +1,39 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+function randomBackground() {
+    var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+    var randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+    body.style.background =
+    "linear-gradient(to right, "
+    +"#" + randomColor1
+    + ", "
+    +"#" + randomColor2
+    +")";
+    color1.value = "#" + randomColor1;
+    color2.value = "#" + randomColor2;
+    css.textContent = body.style.background + ";";
+}
+
+window.onload = setGradient;
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomBackground);
